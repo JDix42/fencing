3db81bb46373e64e79e20eace82c5be11421cf64
James Dix <james.dix@postgrad.manchester.ac.uk>
2017-11-03 16:50:14 +0000

    Added duplicate script
    Added extra lines to determine whether there are two entries with the same name within the BFA rankings.

--- tidy_bfaid.sql
+++ tidy_bfaid.sql
@@ -29,3 +29,10 @@ SELECT * FROM fencing_project.dbo.BFA_ID;
 
 SELECT * FROM fencing_project.dbo.BFA_ID
 WHERE FirstName = 'Lachlan';
+
+SELECT BFA1.BFA_ID, BFA1.Surname, BFA1.FirstName, BFA1.Country, BFA1.Club, BFA1.NIF_Val 
+FROM fencing_project.dbo.BFA_ID AS BFA1
+JOIN fencing_project.dbo.BFA_ID AS BFA2
+ON BFA1.Surname = BFA2.Surname AND BFA1.FirstName = BFA2.FirstName
+WHERE BFA1.BFA_ID != BFA2.BFA_ID
+ORDER BY BFA1.Surname;
