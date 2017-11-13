ccf76df7978e734612780987ad0a8ce3025e0d5b
James Dix <james.dix@postgrad.manchester.ac.uk>
2017-11-13 14:19:16 +0000

    Added Dates and No Duplicates
    Added section that checks the date of a comeptition to decide on the ranking set that should be used for that competition ( a new set comes out every 6 months).
    
    Additionally, this script now provides the BF Iicence number and NIF ranking points without any duplicates.

--- ct_add_bfavals.sql
+++ ct_add_bfavals.sql
@@ -1,9 +1,63 @@
 /* This query inserts BFA ID and NIF points 
 to a competition table (ct) */
-DROP VIEW BfaTemp
+DROP VIEW BTemp
 GO
 
-CREATE VIEW BfaTemp (RankID, LN, FN, BFN, Bfa_ID, NifVals)
+/* Create temporary table to determine any duplicates */
+CREATE VIEW BTemp (RankID, LN, FN, BFN, Bfa_ID, NifVals)
+AS
+(SELECT ct.Rank, ct.LastName, ct.Firstname, bfa.FirstName AS BfaName, bfa.BFA_ID, bfa.NIF_Val
+FROM dbo.birm_res_new AS CT
+LEFT JOIN dbo.BFA_ID as BFA
+ON UPPER(ct.Lastname) = UPPER(bfa.Surname)
+AND LTRIM(UPPER(ct.FirstName)) = LTRIM(UPPER(bfa.FirstName)))
+GO
+
+/* Determine which BFA ranking set to use.
+New rankings sets with different NIF values.
+The rankings are updated every September and 
+March each year */
+DROP TABLE #BFA_set
+
+CREATE TABLE #BFA_set (
+BFA_ID	FLOAT,
+Surname		NVARCHAR(255),
+FirstName	NVARCHAR(255),
+Country		NVARCHAR(255),
+Club		NVARCHAR(255),
+NIF_Val		FLOAT,
+MidName		NVARCHAR(255),
+PosID		INT)
+
+DECLARE @Comp_date datetime = (SELECT Date FROM dbo.Comp WHERE Comp_ID = 1)
+DECLARE @date1 datetime = '2017-09-01 00:00:00.000'
+DECLARE @date2 datetime = '2017-03-01 00:00:00.000'
+DECLARE @date3 datetime = '2016-09-01 00:00:00.000'
+
+IF @Comp_date >= @date1
+	BEGIN
+		PRINT 'Set1'
+		INSERT INTO #BFA_set
+		SELECT *
+		FROM dbo.BFA_ID
+	END
+ELSE IF @Comp_date >= @date2
+	BEGIN
+		PRINT 'Set2'
+		INSERT INTO #BFA_set
+		SELECT *
+		FROM dbo.BFA_IDMar2017
+	END
+ELSE
+	BEGIN 
+		PRINT 'Set3'
+		INSERT INTO #BFA_set
+		SELECT *
+		FROM dbo.BFA_IDSept2016
+	END ;		
+
+/* Create CTE for BFA IDs and NIF values. */
+WITH BfaTemp (RankID, LN, FN, BFN, Bfa_ID, NifVals)
 
 AS
 --(SELECT bfa.BFA_ID, bfa.NIF_Val
@@ -12,15 +66,10 @@ AS
 --ON UPPER(ct.LastName) = UPPER(bfa.Surname))
 (SELECT ct.Rank, ct.LastName, ct.Firstname, bfa.FirstName AS BfaName, bfa.BFA_ID, bfa.NIF_Val
 FROM dbo.birm_res_new AS CT
-LEFT JOIN dbo.BFA_ID as BFA
+LEFT JOIN #BFA_set as BFA
 ON UPPER(ct.Lastname) = UPPER(bfa.Surname)
-AND LTRIM(UPPER(ct.FirstName)) = LTRIM(UPPER(bfa.FirstName)))
-
-GO
-
-DELETE FROM BfaTemp
-WHERE BFA_ID in
-/* Determine BFA ID and Position in ranking based on first and last names*/
+AND LTRIM(UPPER(ct.FirstName)) = LTRIM(UPPER(bfa.FirstName))
+WHERE BFA.BFA_ID NOT IN
 (SELECT BFA.BFA_ID as DelID 
 FROM dbo.BFA_ID AS BFA
 LEFT JOIN 
@@ -28,15 +77,32 @@ LEFT JOIN
 FROM
 /* Determine any names where that are duplicated in the table */
 (SELECT BfaT.FN AS FirstName, BfaT.LN As LastName, COUNT(BfaT.LN) AS Replicas
-FROM BfaTemp AS BfaT
+FROM BTemp AS BfaT
 GROUP BY BfaT.FN, BfaT.LN
 HAVING COUNT(BfaT.LN) >= 2 AND COUNT(BfaT.FN) >= 2) AS DupNames
-LEFT JOIN BFA_ID AS BFA
+LEFT JOIN #BFA_set AS BFA
 ON DupNames.LastName = BFA.Surname AND DupNames.FirstName = BFA.FirstName
 GROUP BY DupNames.LastName, DupNames.FirstName) AS DelList
 ON BFA.PosID = DelList.DeleteID
-WHERE DelList.FirstName IS NOT NULL);
-GO
+WHERE DelList.FirstName IS NOT NULL))
+
+SELECT * FROM BfaTemp
+
+/* Ensure that previous attemps to update BFA_ID and BF_points
+have been removed and changed to NULL */
+UPDATE dbo.birm_res_new
+SET BFA_ID = NULL, BF_points = NULL
+
+/* Update values for BFA_ID and BF_points */
+MERGE INTO dbo.birm_res_new AS BRN
+USING BfaTemp AS BfaT
+ON (BRN.LastName = BfaT.LN
+AND BRN.FirstName = BfaT.FN)
+WHEN MATCHED THEN 
+UPDATE SET
+BRN.BFA_ID = BfaT.BFA_ID,
+BRN.BF_points = BfaT.NifVals;
+
 /* TEST for whether Surnames do still exist in BFA ID database
 SELECT * FROM BFA_ID
 JOIN (SELECT LN, FN 
