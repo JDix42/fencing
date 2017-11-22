c0aaa0c9d11f57a23d1c241ed067eb78aa5c9e9c
James Dix <james.dix@postgrad.manchester.ac.uk>
2017-11-22 10:58:57 +0000

    Added Dom or Int Column
    Added a column to the competition folder to determine whether the competition is a domestic or an international competition. This is needed when the overall results are added to determine the general rankings.

--- add_to_comptable.sql
+++ add_to_comptable.sql
@@ -2,18 +2,21 @@ DECLARE @LongName nvarchar(255) = 'Birmingham International Fencing';
 DECLARE @ShortName nvarchar(8) = 'BirmIntl';
 DECLARE @Date datetime = '2017-04-15 00:00:00.000';
 DECLARE @TotalNumFencers Int = '117';
+DECLARE @DomOrInt nvarchar(1) = 'D'
 
 INSERT INTO dbo.Comp (
 CompName,
 ShortName,
 Date,
-TotalNumFencers
+TotalNumFencers,
+DomOrInt
 )
 
 VALUES (@LongName,
 		@ShortName
 		@Date,
-		@TotalNumFencers);
+		@TotalNumFencers,
+		@DomOrInt);
 
 UPDATE dbo.Comp
 SET FencerCutOff = (FLOOR( 0.75 * @TotalNumFencers))
