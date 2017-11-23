ff61132ed0401b1e0a4bf8a672674ce3f7dd952e
James Dix <james.dix@postgrad.manchester.ac.uk>
2017-11-23 10:26:46 +0000

    Basic Ranking List
    Added a basic ranking list that takes into account all of the results for all of the fencers - it doesn't account for the difference between domestic and international competitions.

--- RankingList.sql
+++ RankingList.sql
@@ -1,2 +1,8 @@
 /* This query creates the rankings for the fencers based on
 the top six domestic "all_results" table */
+
+/* This command generate a basic ranking list for all fencers */
+SELECT FirstName, LastName, SUM(BF_Points) AS TotalPoints, COUNT(FirstName)
+FROM dbo.all_results
+GROUP BY BFA_ID, FirstName, LastName
+ORDER BY TotalPoints DESC;
